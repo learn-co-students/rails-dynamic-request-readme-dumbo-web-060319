d70567a385ff797a3665fb11e5bcd85f3123605d
Sean Padden <you@example.com>
2019-06-26 19:59:38 -0400

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
-
+    
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
