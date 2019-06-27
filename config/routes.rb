71e989a83840413db36c496debbf5d4b26beb941
github username <github email address>
2019-06-27 15:36:53 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get "/url_endpoint/:id", to: "controller_file#controllerMethodConnectingViewPage"
+  resources :posts, only: :show
 end
